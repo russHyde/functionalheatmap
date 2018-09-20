6bc54b69bbca4c770014a37dfb1fb4a793f66340
Russell Hyde <russ_hyde@hotmail.com>
2018-09-20 19:49:13 +0100

    convert the body-df to a matrix in setup_heatmap
    ComplexHeatmap uses matrices as input, so the tidy data-frame (body_data)
    passed into setup_heatmap has to be converted to a matrix.
    
    Adds and tests a function `as_matrix` for converting a data-frame to a matrix.
    This function allows the user to specify a rowname_column which should be
    pulled out of the data-frame and used as rownames in the resulting matrix.
    
    Package now imports dplyr / tidyr / magrittr

---
+++ R/data_manipulation.R
@@ -0,0 +1,23 @@
+###############################################################################
+
+#' converts a data-frame into a matrix
+#'
+#' If the user specifies `rowname_col`, this column is extracted from the
+#' data-frame and used to define the rownames of the matrix
+#'
+#' @importFrom   dplyr         select_
+#' @importFrom   magrittr      set_rownames   %>%
+#' @noRd
+#'
+as_matrix <- function(df, rowname_col = NULL) {
+  if (is.null(rowname_col)) {
+    as.matrix(df)
+  } else {
+    as.matrix(
+      dplyr::select_(df, .dots = paste0("-", rowname_col))
+    ) %>%
+      magrittr::set_rownames(df[[rowname_col]])
+  }
+}
+
+###############################################################################
--- R/setup_heatmap.R
+++ R/setup_heatmap.R
@@ -6,12 +6,33 @@
 
 #' setup_heatmap: defines the data-structures for use in constructing heatmaps
 #'
+#' @param        x             A poly_frame or list of named data-frames. This
+#'   must contain a data-frame called body_data which defines the data used in
+#'   the body of the heatmap (by two index columns and a value column).
+#'
+#' @importFrom   magrittr      %>%
+#' @importFrom   tidyr         spread_
+#'
+#' @include      data_manipulation.R
+#'
 #' @export
+
 setup_heatmap <- function(x) {
+  row_index <- "feature_id"
+  col_index <- "sample_id"
+  value_index <- "fitted_value"
+
   stopifnot(
-    .is_nonempty_list_of_data_frames(x) && "bodydata" %in% names(x)
+    .is_nonempty_list_of_data_frames(x) && "body_data" %in% names(x)
   )
-  x
+  body_matrix <- tidyr::spread_(
+    x$body_data,
+    value_col = value_index,
+    key_col = col_index
+  ) %>%
+    as_matrix(rowname_col = row_index)
+
+  list(body_matrix = body_matrix)
 }
 
 ###############################################################################
