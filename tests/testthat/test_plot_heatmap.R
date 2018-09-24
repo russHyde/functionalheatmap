5f1284fae0fbbd2185a7290983a99d4b48405b3b
Russell Hyde <russ_hyde@hotmail.com>
2018-09-24 19:09:42 +0100

    fixes #9: allow row-annotated heatmaps
    New function `annotate_heatmap` (and helper `.append_row_annotations`) for
    adding rowAnnotations to a heatmap. User must specifiy columns of `row_data`
    to be used as annotation columns.
    
    `plot_heatmap` was modified to respect returning HeatmapLists as well as
    Heatmaps. It checks whethre `row_annotations` entry is defined for a
    heatmap_data object and appends the row-annotations to the heatmap if they do
    exist.
    
    tests:
    
    - `plot_heatmap` checks that output from a row-annotated heatmap is a
    HeatmapList
    
    - `annotate_heatmap` fails on invalid (non heatmap_data) input
    
    - `annotate_heatmap` adds a `row_annotation` data-frame to a heatmap_data
    object (& some validity checks on the row_data entry).

--- tests/testthat/test_plot_heatmap.R
+++ tests/testthat/test_plot_heatmap.R
@@ -19,12 +19,28 @@ test_that("plot_heatmap: invalid input", {
 test_that("plot_heatmap: valid input", {
   # Can't really test this function properly as it plots to the graphics device
 
-  hd1 <- as_heatmap_data(list(body_matrix = matrix(1:4, nrow = 2)))
+  hd1 <- as_heatmap_data(list(
+    body_matrix = matrix(1:4, nrow = 2),
+    row_data = data.frame(sample_id = 1:2, annot = c(TRUE, FALSE))
+  ))
 
   expect_is(
     plot_heatmap(hd1),
     "Heatmap",
-    info = "output from plot_heatmap should be a Heatmap object"
+    info = paste(
+      "output from plot_heatmap should be a Heatmap object if no annotations",
+      "are present"
+    )
+  )
+
+  hd2 <- annotate_heatmap(hd1, row_annotations = "annot")
+  expect_is(
+    plot_heatmap(hd2),
+    "HeatmapList",
+    info = paste(
+      "output from plot_heatmap should be a HeatmapList when annotations are",
+      "present"
+    )
   )
 })
 
