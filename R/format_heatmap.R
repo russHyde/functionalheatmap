2d813d5f22ede3500cf253b9d5bdf7eff8eceef7
Russell Hyde <russ_hyde@hotmail.com>
2018-09-24 16:39:13 +0100

    fixes #14: add clusters/splits in format_heatmap
    Pre-existing clustering of the features in a heatmap can be passed through by
    setting split = c(colname1, colname2, ...) in `format_heatmap`. Here, the
    colnames must be present in the row_data entry of the heatmap_data object.
    
    That the `split` argument isn't passed directly through to Heatmap is annotated
    in the docs for `format_heatmap()`.
    
    Tests:
    
    - single column that is present within `row_data`
    
    - single column that is absent from `row_data` throws an error

--- R/format_heatmap.R
+++ R/format_heatmap.R
@@ -4,13 +4,6 @@
 
 ###############################################################################
 
-.get_default_formatting <- function() {
-  list(
-    show_row_names = FALSE,
-    cluster_columns = FALSE
-  )
-}
-
 # Default formatting: no rownames, no column clustering
 
 #' format_heatmap
@@ -18,7 +11,9 @@
 #' @param        x             A heatmap_data object. As returned by
 #'   `setup_heatmap`.
 #' @param        ...           Any user-specific formatting flags to be passed
-#'   to Heatmap().
+#'   to Heatmap(). An exception is `split`: if this flag is set it should
+#'   define a vector of column-names from the `row_data` part of `x`, these
+#'   columns are then extracted and passed to Heatmap().
 #'
 #' @importFrom   methods       is
 #' @export
@@ -28,17 +23,50 @@ format_heatmap <- function(x, ...) {
     stop("`x` should be a `heatmap_data` object in `format_heatmap`")
   }
 
-  formats <- list(...)
-  defaults <- .get_default_formatting()
+  dots <- list(...)
 
-  formats <- append(
-    formats,
-    defaults[!names(defaults) %in% names(formats)]
-  )
+  formats <- .get_default_formatting() %>%
+    .append_format_args(dots) %>%
+    .append_split_args_if_defined(x, dots$split)
 
   x %>%
     append(list(formats = formats)) %>%
     as_heatmap_data()
 }
+
+###############################################################################
+
+.get_default_formatting <- function() {
+  list(
+    show_row_names = FALSE,
+    cluster_columns = FALSE
+  )
+}
+
+###############################################################################
+
+.append_format_args <- function(current_args, new_args) {
+  non_updated_args <- setdiff(names(current_args), names(new_args))
+  append(new_args, current_args[non_updated_args])
+}
+
+.get_split_from_row_data <- function(x, split_columns) {
+  stopifnot("row_data" %in% names(x))
+  row_data <- x$row_data
+  row_data[split_columns]
+}
+
+.append_split_args_if_defined <- function(current_args,
+                                          heatmap_data,
+                                          split_columns = NULL) {
+  if (is.null(split_columns)) {
+    return(current_args)
+  }
+  .append_format_args(
+    current_args,
+    list(split = .get_split_from_row_data(heatmap_data, split_columns))
+  )
+}
+
 
 ###############################################################################
